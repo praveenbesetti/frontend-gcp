1a32af8b34df24e0d3814b5689ffb1b73540c73e
praveen <135096397+praveenbesetti@users.noreply.github.com>
2026-03-01 17:19:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const res = await axios.post('http://api-docker-backend.duckdns.org/api/users', formData);
-    console.log("fsdvsv",res);
+    console.log("fsdvsdhghgfhv",res);
     setItems([...items, res.data]); // Update UI
     setFormData({ title: '', description: '' }); // Reset form
   };
